8f0f514de125199597b62c0feb8247a35ee13728
Maulana Imamul Khaq <mm930199@gmail.com>
2022-07-21 11:54:05 +0700

    fixing env port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -16,7 +16,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(CLIENT_PATH, 'index.html'));
 });
 
-const port = process.env.port || 3333;
+const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log('Listening at http://localhost:' + port);
 });
