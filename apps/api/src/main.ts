32b5c1c269d1b05ae05fc142d88955baf38e9c40
Maulana Imamul Khaq <mm930199@gmail.com>
2022-07-21 11:35:58 +0700

    client build path

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,25 +1,19 @@
 import * as express from 'express';
 import * as path from 'path';
 import { Message } from '@realtime-chat/api-interfaces';
-import { dirname } from 'path';
 
+const CLIENT_PATH = path.join(__dirname, '../react');
 const app = express();
+app.use(express.static(CLIENT_PATH));
 
 const greeting: Message = { message: 'Welcome to api!' };
-// console.log(path.join(__dirname, '../react/index.html'));
 
-// app.use(
-//   '/home',
-//   express.static(path.join(__dirname, '..', '/react/index.html'))
-// );
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', '/react/index.html'));
-// });
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../react/index.html'));
-// });
 app.get('/api', (req, res) => {
   res.send(greeting);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(CLIENT_PATH, 'index.html'));
 });
 
 const port = process.env.port || 3333;
